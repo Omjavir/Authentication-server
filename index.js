a3a4b7cb4dc47cd4c298d8bbb44cfdc1bc8ae09f
Om Suresh Javir <javirom07042003@gmail.com>
2022-11-08 20:25:56 +0530

    Removed code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const db = require('./config/database')
 const userRoutes = require('./routes/userRoutes')
 const quoteRoutes = require('./routes/userRoutes')
-const path = require('path')
+// const path = require('path')
 require('dotenv').config();
 // const { isUserAuthenticated } = require('./middleware/auth')
 
@@ -15,21 +15,21 @@ app.use(cors())
 // CONNECTION TO DATABASE
 db()
 
-// VERCEL DEPLOYMENT
-const env = 'production'
-if (env === 'production') {
-    app.use(express.static(path.join(__dirname, "../frontend/build")));
-    app.get("*", function (_, res) {
-        res.sendFile(
-            path.join(__dirname, "../frontend/build/index.html"),
-            function (err) {
-                if (err) {
-                    res.status(500).send(err)
-                }
-            }
-        )
-    })
-}
+// // VERCEL DEPLOYMENT
+// const env = 'production'
+// if (env === 'production') {
+//     app.use(express.static(path.join(__dirname, "../frontend/build")));
+//     app.get("*", function (_, res) {
+//         res.sendFile(
+//             path.join(__dirname, "../frontend/build/index.html"),
+//             function (err) {
+//                 if (err) {
+//                     res.status(500).send(err)
+//                 }
+//             }
+//         )
+//     })
+// }
 
 app.get('/', (req, res) => {
     res.send('Hello World')
